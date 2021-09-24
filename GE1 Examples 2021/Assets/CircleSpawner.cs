eb9505077dabf214b833bd3dd2ab61cd9564395e
Evan Williams <evskimo97@gmail.com>
2021-09-24 16:41:48 +0100

    ROTOTOTOATION
    ROTATION WOOOOO

---
+++ GE1 Examples 2021/Assets/CircleSpawner.cs
@@ -0,0 +1,46 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+ using System.Runtime.Serialization;
+
+ using UnityEngine;
+
+public class CircleSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Material colorMaterial;
+    
+    public float radius = 20;
+    public int points = 1;
+
+    public float ringSpacing = 0.25f;
+    public int rings = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    for (int ringNum = 1; ringNum < rings; ringNum++) {
+	        radius = ringNum * ringSpacing;
+	        points += 2 * ringNum;
+	        for (int i = 0 ; i < points ; i ++) {
+		        float theta  = (Mathf.PI * 2.0f) / (float) points;
+		        float angle = theta * i;
+		        Vector3 position = new Vector3(
+			        Mathf.Sin(angle) * radius,
+			        Mathf.Cos(angle) * radius,
+			        0
+		        );
+		        GameObject obj = GameObject.Instantiate(prefab, position, Quaternion.identity);
+		        obj.AddComponent<RotateMe>().rotSpeed = 50f;
+		        obj.GetComponent<Renderer>().material = colorMaterial;
+		        obj.transform.parent = transform;
+	        }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GE1 Examples 2021/Assets/RotateMe.cs
+++ GE1 Examples 2021/Assets/RotateMe.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class RotateMe : MonoBehaviour
 {
+    public float rotSpeed;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-    
+        transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
     }
 }
